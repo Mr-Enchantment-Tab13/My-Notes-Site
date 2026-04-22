8edea9491eeede2ccdb482dfbff0e117c2bb8869
Demetrius Demarcus Barthalamew The Third Jr. <decash09@icloud.com>
2026-04-22 15:07:18 +0000

    Added fonts

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/about.html')
+app.get('/artists', (req, res) => {
+  res.sendFile(__dirname + '/public/artists.html')
 })
 
 app.get('/contact', (req, res) => {
